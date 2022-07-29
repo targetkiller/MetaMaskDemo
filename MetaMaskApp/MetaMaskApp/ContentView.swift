bc064cefdc9dab566e2f367880168bc12301ac09
Caroline <targetkillering@gmail.com>
2022-07-29 18:36:01 +0800

    feat: initial

---
+++ MetaMaskApp/MetaMaskApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetaMaskApp
+//
+//  Created by Caroline on 2022/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetaMaskApp/MetaMaskApp/MetaMaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMaskAppApp.swift
+//  MetaMaskApp
+//
+//  Created by Caroline on 2022/7/29.
+//
+
+import SwiftUI
+
+@main
+struct MetaMaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
